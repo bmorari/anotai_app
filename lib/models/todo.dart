3d6f567fcdfd0ff358fc6dbc7b99eebfae9336b4
Bruno A. Morari <brunomorari69@gmail.com>
2026-07-28 22:17:20 -0300

    feat: add completion functionality

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,19 +1,20 @@
 class Todo {
-
-  Todo({required this.title, required this.dateTime});
+  Todo({required this.title, required this.dateTime, this.completed = false});
 
   Todo.fromJson(Map<String, dynamic> json)
     : title = json['title'],
-      dateTime = DateTime.parse(json['dateTime']);
+      dateTime = DateTime.parse(json['dateTime']),
+      completed = json['completed'] ?? false;
 
   String title;
   DateTime dateTime;
+  bool completed = false;
 
   Map<String, dynamic> toJson() {
     return {
       'title': title,
-      'dateTime': dateTime.toIso8601String()
+      'dateTime': dateTime.toIso8601String(),
+      'completed': completed,
     };
   }
-
 }
